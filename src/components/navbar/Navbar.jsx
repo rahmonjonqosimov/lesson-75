a6712ec66180f49f1b6081774382bc5f65c216e0
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-05-11 00:36:53 +0500

    responsive and readme added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FiMenu } from "react-icons/fi";
 
 function Navbar() {
+  const [menu, setMenu] = useState(false);
   let users = useSelector((state) => state.users.value);
   return (
     <div className="navbar">
       <h2>Redux Toolkit</h2>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"/create-user"}>Create user</NavLink>
-      <NavLink to={"/all-users"}>
-        All users <sup>{users.length}</sup>
-      </NavLink>
+      <div className={`link ${menu ? "show" : ""}`}>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/create-user"}>Create user</NavLink>
+        <NavLink to={"/all-users"}>
+          All users <sup>{users.length}</sup>
+        </NavLink>
+      </div>
+      <FiMenu onClick={() => setMenu(!menu)} className="menu" />
     </div>
   );
 }
